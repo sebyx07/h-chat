58194ed74f38d78b4a3a29832e9a6d29941ed610
sebi <gore.sebyx@yahoo.com>
2015-12-06 19:47:09 +0200

    add moment and change the messages

--- h_chat/app/routes/chat.js
+++ h_chat/app/routes/chat.js
@@ -3,7 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params){
     return Ember.RSVP.hash({
-      username: params.username
+      username: params.username,
+      messages: this.messages()
     });
+  },
+
+  messages: function(){
+    return [
+      {
+        payload: 'payload1',
+        user: {
+          username: 'user1'
+        }
+      },
+      {
+        payload: 'payload2',
+        user: {
+          username: 'user2'
+        }
+      }
+    ]
   }
 });
